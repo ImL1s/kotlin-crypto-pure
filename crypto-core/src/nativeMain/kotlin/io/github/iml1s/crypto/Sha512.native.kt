29d35b20779a93c105c5369bf907c6242bca9406
ImL1s <aa22306546@hotmail.com>
2026-01-05 08:46:42 +0800

    fix(native): correct SHA512 class name casing

--- crypto-core/src/nativeMain/kotlin/io/github/iml1s/crypto/Sha512.native.kt
+++ crypto-core/src/nativeMain/kotlin/io/github/iml1s/crypto/Sha512.native.kt
@@ -1,7 +1,7 @@
 package io.github.iml1s.crypto
 
-import org.kotlincrypto.hash.sha2.Sha512
+import org.kotlincrypto.hash.sha2.SHA512
 
 actual fun platformSha512(data: ByteArray): ByteArray {
-    return Sha512().digest(data)
+    return SHA512().digest(data)
 }
