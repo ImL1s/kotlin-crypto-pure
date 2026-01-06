54e6c885b08416cffc96f9561314e7001cf8cb7f
ImL1s <aa22306546@hotmail.com>
2026-01-07 02:58:15 +0800

    chore: Enable Solana dependency with correct naming

--- kotlin-wallet-sdk/build.gradle.kts
+++ kotlin-wallet-sdk/build.gradle.kts
@@ -15,7 +15,7 @@ kotlin {
                 implementation("io.github.iml1s:kotlin-tx-builder:1.3.0")
                 implementation("io.github.iml1s:kotlin-utxo:1.3.0")
                 implementation("io.github.iml1s:kotlin-address:1.3.0")
-                // Note: kotlin-solana excluded due to dependency resolution issues
+                implementation("io.github.iml1s:solana:1.3.0")
                 implementation(libs.kotlinx.coroutines.core)
                 implementation(libs.ktor.client.core)
                 implementation(libs.ktor.client.content.negotiation)
